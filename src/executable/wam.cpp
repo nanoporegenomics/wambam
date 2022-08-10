3b2deceb4276025411f3ecc65b7cc6b6e6de702d
Ryan <rlorigro@ucsc.edu>
2022-08-10 15:35:58 -0700

    More CLI

--- src/executable/wam.cpp
+++ src/executable/wam.cpp
@@ -3,27 +3,38 @@
 #include "Bam.hpp"
 
 using ghc::filesystem::path;
+using ghc::filesystem::exists;
+using ghc::filesystem::create_directories;
 using gfase::SamElement;
 using gfase::Bam;
 
 #include <unordered_map>
 #include <stdexcept>
 #include <iostream>
+#include <fstream>
 #include <string>
 
 using std::unordered_map;
 using std::runtime_error;
+using std::ofstream;
 using std::cerr;
 using std::string;
 
 
-void get_identity_from_bam(path bam_path){
+void get_identity_from_bam(path bam_path, path output_dir){
+    if (exists(output_dir)){
+        throw runtime_error("ERROR: output directory exists already");
+    }
+    else {
+        create_directories(output_dir);
+    }
+
     Bam bam_reader(bam_path);
 
     unordered_map<int64_t, int64_t> identity_ppm_distribution;
 
     bam_reader.for_alignment_in_bam(true, [&](SamElement& e){
-        cerr << e.ref_name << ' ' << e.query_name << ' ' << int(e.mapq) << ' ' << e.flag << '\n';
+//        cerr << e.ref_name << ' ' << e.query_name << ' ' << int(e.mapq) << ' ' << e.flag << '\n';
 
         int64_t matches = 0;
         int64_t nonmatches = 0;
@@ -42,19 +53,19 @@ void get_identity_from_bam(path bam_path){
 
         int64_t identity_ppm = (matches * 1000000) / (nonmatches + matches);
 
-        cerr << matches << ' ' << nonmatches << ' ' << identity_ppm << ' ' << double(identity_ppm)/10000 << '\n';
-
         identity_ppm_distribution[identity_ppm] += 1;
     });
 
+    ofstream file(output_dir / "identity_distribution.csv");
     for (auto& [ppm,count]: identity_ppm_distribution){
-        cerr << double(ppm)/10000 << ' ' << count << '\n';
+        file << double(ppm)/10000 << ',' << count << '\n';
     }
 }
 
 
 int main (int argc, char* argv[]){
     path bam_path;
+    path output_dir;
 
     CLI::App app{"App description"};
 
@@ -64,9 +75,15 @@ int main (int argc, char* argv[]){
             "Path to BAM")
             ->required();
 
+    app.add_option(
+            "-o,--output_dir",
+            output_dir,
+            "Path to directory which will be created for output (must not exist already)")
+            ->required();
+
     CLI11_PARSE(app, argc, argv);
 
-    get_identity_from_bam(bam_path);
+    get_identity_from_bam(bam_path, output_dir);
 
     return 0;
 }
